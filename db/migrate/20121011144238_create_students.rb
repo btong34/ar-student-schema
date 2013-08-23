4bfe605b991680950b7a104a669ea3e9292c3bdd
Brian Tong <btong34@gmail.com>
2013-08-22 23:39:30 -0500

    .

--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -14,4 +14,23 @@ class CreateStudents < ActiveRecord::Migration
       t.date :birthday
     end
   end
+  
+  def up
+    add_column :students, :name, :string
+    add_column :students, :address, :string
+    Student.each do |student|
+      student.update_attributes! :name => student.first_name + " " + student.last_name
+    end
+    remove_column :students, :first_name
+    remove_column :students, :last_name
+  end
+
+  def down
+    add_column :first_name, :last_name
+    Student.each do |student|
+      student.update_attributes! :first_name => student.name.split(" ")[0]
+      student.update_attributes! :last_name => student.name.split(" ")[1]
+    end
+    remove_column :name
+  end
 end
